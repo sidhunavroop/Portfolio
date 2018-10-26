e1cd87619c6dff9394c34d28c254a46505001034
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2018-10-26 14:41:18 -0400

    integrated new and create action for portfolios

--- app/controllers/personalportfolios_controller.rb
+++ app/controllers/personalportfolios_controller.rb
@@ -2,4 +2,27 @@ class PersonalportfoliosController < ApplicationController
   def index
     @portfolios = Personalportfolio.all
   end
+
+  def new
+    @portfolio = Personalportfolio.new
+  end
+
+  def create
+    @portfolio = Personalportfolio.new(params.require(:personalportfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to personalportfolios_path, notice: 'Portfolio was successfully created.' }
+        
+      else
+        format.html { render :new }
+        
+      end
+    end
+  end
+
+
+
+
+
 end
