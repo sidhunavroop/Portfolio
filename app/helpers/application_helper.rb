0d0ca14c0737afaf0b764c7eec956258d2524c8f
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2019-06-06 00:05:51 -0400

    Refactored alerts in helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,5 +57,18 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Navroop Singh Sidhu's Portfolio", sticky: false)
+  end
+
 end
 
