313007183f627f17c30bdbde942f7aa7dffe7e7b
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2019-06-07 19:22:43 -0400

    Created a comment broadcast, configured the webcast route and updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -19,6 +18,9 @@ Rails.application.routes.draw do
       get :toggle_status 
     end
   end
+
+  mount ActionCable.server => '/cable'
+
   root to:'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
