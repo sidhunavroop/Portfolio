7fe2e38145a1c32efd3f77e4e49efb74beb56126
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2019-06-11 01:00:49 -0400

    Implemented twitter call on raptor news page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   
   get 'about', to:'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
   resources :blogs do
     member do
       get :toggle_status 
