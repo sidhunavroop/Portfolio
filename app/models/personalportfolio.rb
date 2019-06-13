49d170da550cd3a6be2a2110818cb668ff78fd6e
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2019-06-13 16:46:55 -0400

    Fixed portfolio view error and added progress bars for skills

--- app/models/personalportfolio.rb
+++ app/models/personalportfolio.rb
@@ -3,7 +3,7 @@ class Personalportfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 allow_destroy: true,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
-  include Placeholder
+  
   validates_presence_of :title, :body
   mount_uploader :thumb_image, PersonalportfolioUploader
   mount_uploader :main_image, PersonalportfolioUploader
@@ -19,9 +19,4 @@ class Personalportfolio < ApplicationRecord
 
   scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
 
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
-  
 end
